a37662cb27dcd622e9719d216e1f6a0869c6e8b4
Vawkay <100175396+Vawkay@users.noreply.github.com>
2022-02-27 13:06:58 -0300

    Add files via upload

---
+++ exercise_areas.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercise_areas {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+	
+		
+		double A, B, C;
+		double pi, triangulo, circulo, trapezio, quadrado, retangulo;
+		
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		C = sc.nextDouble();
+
+		
+		pi = 3.14159;
+		triangulo = (A * C)/2.0;
+		circulo = pi * Math.pow(C, 2);
+		trapezio = ((A+B)*C)/2.0;
+		quadrado = B * B;
+		retangulo = A * B;
+		
+		System.out.printf("TRIANGULO: %.3f%n", triangulo);
+		System.out.printf("CIRCULO: %.3f%n", circulo);
+		System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+		System.out.printf("QUADRADO: %.3f%n", quadrado);
+		System.out.printf("RETANGULO %.3f%n", retangulo);
+		
+		sc.close();
+	}
+
+}

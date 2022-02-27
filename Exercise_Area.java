8f98861ffcfca06bb79156b6c886cb9ee6cc592a
Vawkay <100175396+Vawkay@users.noreply.github.com>
2022-02-27 10:45:39 -0300

    Add files via upload

---
+++ Exercise_Area.java
@@ -0,0 +1,31 @@
+		import java.util.Locale;
+		import java.util.Scanner;
+		
+public class Exercise {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		double raio;
+		double area;
+		double pi;
+		double a;
+		
+		pi = 3.14159;
+		
+		raio = sc.nextDouble();
+		sc.nextLine();
+		
+		a = Math.pow(raio, 2);
+		area = pi*a;
+		
+		System.out.printf("O valor da área é de: %.4f%n", area);
+		
+		
+		sc.close();
+		
+	}
+
+}
